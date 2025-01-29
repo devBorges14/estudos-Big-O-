6d51363a6f2f13c444cba32dd391c09c1be45986
Gabriel Borges <140124714+devBorges14@users.noreply.github.com>
2025-01-29 17:00:00 -0300

    Create O(1).py

---
+++ O(1).py
@@ -0,0 +1,6 @@
+def pegar_primeiro(lista): # Criando a função
+    return lista[0]  # Sempre acessa o primeiro item
+
+brinquedos = ["ursinho", "boneca", "carrinho", "bola", "lapis", "massinha", "caderno", "aquarela", "dino"] # Lista de brinquedos para pegar o primeiro
+print(pegar_primeiro(brinquedos))  # Saída: ursinho 
+# Sempre rápido, não importa o tamanho da lista!
